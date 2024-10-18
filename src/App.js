1385bba339899f46b84715a1ebb89fbb9f5177d0
Edo Mažar <edoxyz3@gmail.com>
2024-10-18 18:45:12 +0200

    zadaca

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { UserChildren, UserClass, UserFunction } from "./user";
 import { Gumb } from "./user/Gumb";
 
@@ -9,53 +9,45 @@ import { Gumb } from "./user/Gumb";
 //   { name: "Ana", years: 25 },
 // ];
 
-export default class App extends React.Component {
-  state = {
-    users: [
-      { name: "Ivan", years: 30 },
-      { name: "Marko", years: 35 },
-      { name: "Ana", years: 25 },
-    ],
-    child: "A hobi mi je sviranje gitare",
-  };
+export default function App() {
+  const [users, setUsers] = useState([
+    { name: "Ivan", years: 30 },
+    { name: "Marko", years: 35 },
+    { name: "Ana", years: 25 },
+  ]);
 
-  handleButtonPress = () => {
-    const { users } = this.state;
-    const ageIncrease = Math.random().toFixed(1) * 10;
-    console.log(ageIncrease);
+  // const [child, setChild] = useState("A hobi mi je sviranje gitare");
 
+  const child = "A hobi mi je sviranje gitare";
+
+  const handleButtonPress = () => {
     const newUser = users.map((user) => {
+      const ageIncrease = Math.random().toFixed(1) * 10;
+      console.log(ageIncrease);
       return {
         ...user,
         years: user.years + ageIncrease,
       };
     });
-    this.setState({ users: newUser });
+
+    setUsers(newUser);
   };
 
-  render() {
-    const { users } = this.state;
-    return (
-      <div className="App App-header">
-        <h1>React aplikacija</h1>
-        <p>Ovo zbilja radi</p>
+  return (
+    <div className="App App-header">
+      <h1>React aplikacija</h1>
+      <p>Ovo zbilja radi</p>
 
-        {/* <button onClick={this.handleButtonPress}>Promjeni Godine</button> */}
-        <Gumb onClick={this.handleButtonPress} />
+      {/* <button onClick={handleButtonPress}>Promjeni Godine</button> */}
 
-        <UserFunction name={users[0].name} years={users[0].years} />
-        <UserFunction
-          name={this.state.users[1].name}
-          years={this.state.users[1].years}
-        />
-        <UserClass
-          name={this.state.users[2].name}
-          years={this.state.users[2].years}
-        />
-        <UserChildren name="Edo" years={100}>
-          {this.state.child}
-        </UserChildren>
-      </div>
-    );
-  }
+      <Gumb onClick={handleButtonPress} />
+
+      <UserFunction name={users[0].name} years={users[0].years} />
+      <UserFunction name={users[1].name} years={users[1].years} />
+      <UserClass name={users[2].name} years={users[2].years} />
+      <UserChildren name="Edo" years={100}>
+        {child}
+      </UserChildren>
+    </div>
+  );
 }
--- src/user/UserChildren.js
+++ src/user/UserChildren.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export default function UserChildren({ name, years, children }) {
+  //po inputu promjeniti ime
+  //dovrsiti
+  const [names, setNames] = useState("");
+
+  const handleChange = (event) => {
+    setNames(event.target.value);
+  };
+
   return (
     <div>
+      <input
+        type="text"
+        value={names}
+        placeholder="Enter name:"
+        onChange={handleChange}
+      ></input>
       <p>
         Pozdrav, moje ime je {name} i imam {years} godina.
       </p>
